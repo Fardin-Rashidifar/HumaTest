7ae83f634f980d9f8142a35753927bdae28c0ea5
Fardin_Rashidifar <fardinrashidifar75@gmail.com>
2022-08-06 22:17:03 +0430

    customize movie class

--- app/src/main/java/f/r/humatest2/dataClass/Movie.kt
+++ app/src/main/java/f/r/humatest2/dataClass/Movie.kt
@@ -1,16 +1,7 @@
 package f.r.humatest2.dataClass
 
-import java.io.Serializable
 
-/**
- * Movie class represents video entity with title, description, image thumbs and video url.
- */
 data class Movie(
     var id: Long = 0,
-    var backgroundImageUrl: String? = null,
     var cardImageUrl: String? = null
-) {
-
-
-
-}
+)
--- app/src/main/java/f/r/humatest2/dataClass/MovieList.kt
+++ app/src/main/java/f/r/humatest2/dataClass/MovieList.kt
@@ -2,8 +2,8 @@ package f.r.humatest2.dataClass
 
 object MovieList {
     val MOVIE_CATEGORY = arrayOf(
-        "Category Zero",
-        "Category One"
+        "دسته اول",
+        "دسته دوم"
     )
 
     val list: List<Movie> by lazy {
@@ -12,13 +12,7 @@ object MovieList {
     private var count: Long = 0
 
     private fun setupMovies(): List<Movie> {
-        val bgImageUrl = arrayOf(
-            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
-            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
-            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
-            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
-            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg"
-        )
+
         val cardImageUrl = arrayOf(
             "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
             "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
@@ -27,11 +21,10 @@ object MovieList {
             "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
         )
 
-        val list = bgImageUrl.indices.map {
+        val list = cardImageUrl.indices.map {
             buildMovieInfo(
 
-                cardImageUrl[it],
-                bgImageUrl[it]
+                cardImageUrl[it]
             )
         }
 
@@ -39,13 +32,10 @@ object MovieList {
     }
 
     private fun buildMovieInfo(
-        cardImageUrl: String,
-        backgroundImageUrl: String
-    ): Movie {
+        cardImageUrl: String): Movie {
         val movie = Movie()
         movie.id = count++
         movie.cardImageUrl = cardImageUrl
-        movie.backgroundImageUrl = backgroundImageUrl
         return movie
     }
 }
