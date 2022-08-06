a2e4bc9091f8eeba1ff2e4f4fabe0f478019e3ef
Fardin_Rashidifar <fardinrashidifar75@gmail.com>
2022-08-06 22:14:57 +0430

    done G class of app

--- app/src/main/java/f/r/humatest2/androidWrapper/G.kt
+++ app/src/main/java/f/r/humatest2/androidWrapper/G.kt
@@ -1,22 +1,8 @@
-package f.r.f.hamrahanandroidkotlin.androidWrapper
+package f.r.humatest2.androidWrapper
 
-import android.Manifest
 import android.app.Application
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.app.ActivityCompat
 import dagger.hilt.android.HiltAndroidApp
-import f.r.f.hamrahanandroidkotlin.BuildConfig
-import f.r.f.hamrahanandroidkotlin.R
-import f.r.f.hamrahanandroidkotlin.Utils
-import f.r.f.hamrahanandroidkotlin.helper.SendingLocationToServerService
-import ir.atitec.everythingmanager.manager.FontManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
-import timber.log.Timber
+
 
 @HiltAndroidApp
 class G : Application() {
@@ -27,12 +13,6 @@ class G : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-
-        //  FontManager.init(this,R.string.myFont);
-        FontManager.init(this, R.string.defaultFontName)
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
 
 
 
