e05cad73eb77a731a1ddd9c451bc79917aea02ca
Fardin_Rashidifar <fardinrashidifar75@gmail.com>
2022-08-06 22:15:26 +0430

    use data binding in main activity

--- app/src/main/java/f/r/humatest2/view/MainActivity.kt
+++ app/src/main/java/f/r/humatest2/view/MainActivity.kt
@@ -1,17 +1,22 @@
 package f.r.humatest2.view
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import dagger.hilt.android.AndroidEntryPoint
 import f.r.humatest2.R
+import f.r.humatest2.databinding.ActivityMainBinding
 
-
+@AndroidEntryPoint
 class MainActivity : FragmentActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            supportFragmentManager.beginTransaction()
                 .replace(R.id.main_browse_fragment, MainFragment())
                 .commitNow()
         }
